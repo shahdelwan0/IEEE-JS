708ab88c8452171bf81179417eda3440fa2b267e
shahdelwan0 <shahdelwan2024@gmail.com>
2026-02-03 01:21:17 +0200

    Task 3

---
+++ W-01/task_3/main.js
@@ -0,0 +1,8 @@
+console.log(
+  "%cElzero%c  %cWeb%c  %cSchool%c",
+  "color: red; font-size: 40px",
+  "",
+  "color: green; font-weight: bold;font-size: 40px",
+  "",
+  "color: blue;font-size: 40px; background-color: blue; color: white ",
+);
