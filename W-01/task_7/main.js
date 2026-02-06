7dcf4b7eb24322838d8c91caa4dbe987c63beda5
shahdelwan0 <shahdelwan2024@gmail.com>
2026-02-06 17:01:15 +0200

    Tasks: 7:10

---
+++ W-01/task_7/main.js
@@ -0,0 +1,21 @@
+let numberOne = 10;
+let numberTwo = 20;
+
+// 1
+console.log("" + numberOne + numberTwo);
+
+// 2
+console.log(typeof ("" + numberOne + numberTwo));
+
+// 3
+console.log(`${numberOne}${numberTwo}`);
+
+// 4
+console.log(typeof `${numberOne}${numberTwo}`);
+
+// 5 + 6
+console.log(numberTwo + "\n" + numberOne);
+
+// 7 + 8
+console.log(`${numberTwo} 
+${numberOne}`);
